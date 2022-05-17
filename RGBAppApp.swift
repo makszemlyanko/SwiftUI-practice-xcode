801b7fcc01e67154a9f4039f5f3692f26f66a12b
kokosDev <maks.kokos.dev@gmail.com>
2022-05-17 12:55:13 +0300

    changing color with three sliders

---
+++ RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Maks Kokos on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
